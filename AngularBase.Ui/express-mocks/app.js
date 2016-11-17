3f78cba9924dad958da280a72b1d848e22df4857
JeremyTurner1976 <jeremyturner1976@eagles.ewu.edu>
2016-11-16 23:06:48 -0700

    Mock Product call based on id

--- AngularBase.Ui/express-mocks/app.js
+++ AngularBase.Ui/express-mocks/app.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(__dirname));
 
+var productFile = require(path.join(__dirname, path.join("mocks", "apiProduct.json")));
+
 app.listen(51493, function () {
   console.log('Express Server listening on port 51493!')
 })
@@ -22,17 +24,28 @@ app.get('/ping', function (req, res) {
 app.get('/api/v0.0.0/products', function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   res.sendFile(path.join(__dirname, path.join("mocks", "apiProducts.json")));
 })
 
 //TODO product api
+app.get('/api/v0.0.0/products/:id', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  var matchingItems = productFile.filter(function (item) {
+      return item["productID"] == req.params.id;
+  });
+  res.send(matchingItems[0]);
+})
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Express path not found');
   err.status = 404;
   next(err);
-});
+})
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
